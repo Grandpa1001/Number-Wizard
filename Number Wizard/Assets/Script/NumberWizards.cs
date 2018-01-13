377c1b1dd9b9c709e6a6db408c869a34723f8bbd
Kamil <kamil.bandzwolek@gmail.com>
2018-01-13 15:11:08 +0100

    add if
    add key to chance variable

--- Number Wizard/Assets/Script/NumberWizards.cs
+++ Number Wizard/Assets/Script/NumberWizards.cs
@@ -13,10 +13,23 @@ public class NumberWizards : MonoBehaviour {
 
         print("The highest number  you can pick is " + max);
         print("The lowest number  you can pick is " + min);
+
+        print("Is the number higher or lower than 500?");
+        print("Up = higher, down = lower, return = equals");
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up arrow pressed");
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down arrow pressed");
+        } else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!");
+        }
+
+    } 
 }
